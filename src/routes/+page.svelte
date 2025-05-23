66e7da74dddca9fb1e214d28c3b78adb1ac57eb0
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2025-05-23 13:22:14 +0200

    added changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,3 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <a href="{base}/about">ABOUT</a>
-
-<h2>About 2 title</h2>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,2 @@
 <h1>ABOUT PAGE</h1>
+<h2>About page 2</h2>
